0144c9aa91a961e1e6349b865cd6b9c10a718d0e
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2022-06-10 20:52:44 -0600

    style: 🚨 fix wpcs violations

--- includes/class-gragrid-api.php
+++ includes/class-gragrid-api.php
@@ -48,9 +48,12 @@ class Gragrid_API {
 	 * @return array|WP_Error
 	 */
 	public function get_lists() {
-		$response = $this->request( '/marketing/lists', array(
+		$response = $this->request(
+			'/marketing/lists',
+			array(
 			'page_size' => 1000,
-		) );
+			)
+		);
 
 		if ( ! $this->is_valid_response( $response, 200 ) ) {
 			return $this->set_error( $response );
